6b87b546eb7ee37d33806d68d7112f0a9374e522
Luke Barker <luke.barker@student.swic.edu>
2022-11-02 19:37:24 -0500

    Add functions to do the conversions

--- src/utils/tryConvert.js
+++ src/utils/tryConvert.js
@@ -1,9 +1,20 @@
-export function tryConvert(temperature, convert) {
+const conversions = {
+  convertToCelsius(fahrenheit) {
+    return ((fahrenheit - 32) * 5) / 9;
+  },
+  convertToFahrenheit(celsius) {
+    return (celsius * 9) / 5 + 32;
+  },
+};
+
+export function tryConvert(temperature, conversion) {
   const input = parseFloat(temperature);
   if (Number.isNaN(input)) {
     return "";
   }
-  const output = convert(input);
+
+  const output = conversions[conversion](input);
+
   const rounded = Math.round(output * 1000) / 1000;
   return rounded.toString();
 }
