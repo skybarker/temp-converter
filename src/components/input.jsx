fd36351ad763833a8c961ca2baecf47b2126e206
Luke Barker <luke.barker@student.swic.edu>
2022-11-02 20:51:40 -0500

    Final updates from class

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -1,3 +1,17 @@
-export default function Input() {
-  throw new Error("Not Implemented");
+import PropTypes from "prop-types";
+import capitalize from "lodash.capitalize";
+
+export default function Input({ label, id, handleChange }) {
+  return (
+    <div>
+      <label htmlFor={id}>{label || capitalize(id)}</label>
+      <input id={id} type="number" onChange={handleChange} />
+    </div>
+  );
 }
+
+Input.propTypes = {
+  label: PropTypes.string,
+  id: PropTypes.string.isRequired,
+  handleChange: PropTypes.func.isRequired,
+};
--- src/components/input.test.jsx
+++ src/components/input.test.jsx
@@ -1,1 +1,29 @@
-it("renders an input with the proper label and id", () => {});
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { vi } from "vitest";
+import Input from "./Input";
+
+it("renders an input with the proper label and id when provided both", () => {
+  render(<Input label="Test" id="test" />);
+
+  const input = screen.getByLabelText("Test");
+  expect(input).toBeInTheDocument();
+});
+
+it("renders an input with the proper label and id using a default for the label", () => {
+  render(<Input id="test" />);
+  const input = screen.getByLabelText("Test");
+  expect(input).toBeInTheDocument();
+});
+
+it("calls a callback whenever we type in the input", () => {
+  const handleChange = vi.mock();
+  const user = userEvent.setup();
+
+  render(<Input id="test" onChange={handleChange} />);
+
+  const input = screen.getByLabelText("Test");
+  user.type(input, "123");
+
+  expect(handleChange).toHaveBeenCalledTimes(3);
+});
